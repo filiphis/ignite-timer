f499029e9ae3183bd896e91e41d6d1afac4d7705
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-08-02 23:43:45 -0300

    Testando temas no styled components

---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+// import { defaultTheme } from './../@types/styled.d'
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-100']};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+
+`
